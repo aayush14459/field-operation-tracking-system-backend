faa10ecb9a2fe194f7663397fd02bca18c4c70d5
aayush14459 <aayushbhavsar01@gmail.com>
2026-02-23 17:07:30 +0530

    database file updated

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/field_ops_db';
 
 const pool = new Pool({
-  connectionString,
+   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
