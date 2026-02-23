59d28e78b7d9e20aed04fc16439b075e561794f3
aayush14459 <aayushbhavsar01@gmail.com>
2026-02-23 17:15:39 +0530

    database file updated latest

--- src/config/db.js
+++ src/config/db.js
@@ -4,14 +4,18 @@ require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const pool = new Pool({
-   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-});
+const pool = isProduction
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+    });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
